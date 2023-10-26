6be42d7019d7ccdd858018b2371f52226e7a34c2
tubagusdylan <tubagus.dylanr@gmail.com>
2023-10-26 17:15:44 +0700

    first commit

---
+++ src/NotesApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NotesApp extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Halo Dunia</h1>
+      </>
+    );
+  }
+}
+
+export default NotesApp;
